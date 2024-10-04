d611e3e62e608fe56f6010ee93b9230a19a84db8
Kirlion13 <kirlion13@gmail.com>
2024-10-04 19:15:18 +0300

    HypatiaAcademy

--- HypatiaAcademy/sqlite.py
+++ HypatiaAcademy/sqlite.py
@@ -19,9 +19,21 @@ c = conn.cursor()
 #c.execute("INSERT INTO basketball_players VALUES ('23', 'Michael Jordan', 15, 6)")
 #c.execute("INSERT INTO basketball_players VALUES ('30', 'Steph Curry', 10, 3)")
 
+
+#c.execute('''CREATE TABLE injuries(
+ #         player_name TEXT,
+  #         injury TEXT,
+  #        assessment_of_cost INTEGER)''')
+
+#c.executemany('''INSERT INTO injuries VALUES (?,?,?)''', [
+ #   ('LeBron James', 'knee braked', 250),
+  #  ('Michael Jordan', 'strained shoulder', 315),
+   # ('Steph Curry', 'concussion', 400)
+#])
+
 c.execute("DELETE FROM basketball_players WHERE rowid NOT IN (SELECT min(rowid) FROM basketball_players GROUP BY player_name, championships, experience, player_number)")
 
-for row in c.execute("SELECT * FROM basketball_players"):
+for row in c.execute("SELECT * FROM injuries"):
     print(row)
 
 conn.commit()
